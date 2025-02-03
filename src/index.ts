572bfa8c96b7caca67106dfd8c1dfb577b21806c
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2025-02-03 12:00:37 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express, { Application } from "express";
 import dbConfig from "./config/db";
 import appConfig from "./app";
+import path from "path"
 
 const app: Application = express();
 // const app = express();
+
+
+app.use("/Uploads", express.static(path.join(__dirname, "Uploads")));
+
 appConfig(app)
 dbConfig()
 
